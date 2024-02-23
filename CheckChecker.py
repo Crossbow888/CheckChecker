ae17003ad0a8a16c659f642bec628940dc5f1029
Crossbow888 <112905280+Crossbow888@users.noreply.github.com>
2024-02-23 07:39:30 +0100

    Update CheckChecker.py

--- CheckChecker.py
+++ CheckChecker.py
@@ -38,6 +38,9 @@ def placepieces(board, pieces):
             while True:
                 x, y = getrandomcoordinates()
                 if board[(x, y)] == '':
+                    if piece == 'P' or piece == 'p':
+                        if x == 1 or x == 8:
+                            continue
                     board[(x, y)] = piece
                     break
                 else:
@@ -46,7 +49,7 @@ def placepieces(board, pieces):
 
 def isWhiteKingInCheck(board):
     colour = 'w'
-    isWhiteKinginDanger = False
+    isKingInCheck = False
     for i in range(1, 9):
         for j in range(1, 9):
             if board[(i, j)] == 'K':
@@ -54,31 +57,33 @@ def isWhiteKingInCheck(board):
             else: 
                 continue
 
-    isKingInCheck = isKingInCheckOnRank(board, WKing, colour) # add subsequent call to next method later
+    isKingInCheck = isKingInCheckOnRank(board, WKing, colour) 
     isKingInCheck = isKingInCheckOnFile(board, WKing, colour)
-    isKingInCheck = isKingInCheckOnDiagonal(board, WKing, colour) # to be implemented
+    isKingInCheck = isKingInCheckOnDiagonal(board, WKing, colour) 
+    # isKingInCheck = isKingInCheckFromKnight(board, WKing, colour) 
 
     return isKingInCheck
 
 def isKingInCheckOnRank(board, kingPos, colour):
     inCheck = False
+    kingPos = list(kingPos)
     if colour == 'w':
         #check to left
         while kingPos[0] - 1 > 0:
             kingPos[0] -= 1
-            if board[kingPos] == '': 
+            if board[kingPos[0], kingPos[1]] == '': 
                 continue
-            elif board[kingPos] != 'q' or board[kingPos] != 'r':
-                return inCheck
+            elif (board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'r') and board[kingPos[0], kingPos[1]] != '':
+                return inCheck == False
             else:
                 inCheck = True
                 break
         #check to right
         while kingPos[0] + 1 <= 8:
             kingPos[0] += 1
-            if board[kingPos] == '': 
+            if board[kingPos[0], kingPos[1]] == '': 
                 continue
-            elif board[kingPos] != 'q' or board[kingPos] != 'r':
+            elif (board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'r') and board[kingPos[0], kingPos[1]] != '':
                 return inCheck
             else:
                 inCheck = True
@@ -87,13 +92,14 @@ def isKingInCheckOnRank(board, kingPos, colour):
 
 def isKingInCheckOnFile(board, kingPos, colour): 
     inCheck = False
+    kingPos = list(kingPos)
     if colour == 'w':
         #check below
-        while kingPos[1] - 1 > 0:
+        while kingPos[1] - 1 > 0 and kingPos[0] - 1 > 0:
             kingPos[0] -= 1
-            if board[kingPos] == '': 
+            if board[kingPos[0], kingPos[1]] == '': 
                 continue
-            elif board[kingPos] != 'q' or board[kingPos] != 'r':
+            elif (board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'r') and board[kingPos[0], kingPos[1]] != '':
                 return inCheck
             else:
                 inCheck = True
@@ -101,15 +107,82 @@ def isKingInCheckOnFile(board, kingPos, colour):
         #check above
         while kingPos[0] + 1 <= 8:
             kingPos[0] += 1
-            if board[kingPos] == '': 
+            if board[kingPos[0], kingPos[1]] == '': 
                 continue
-            elif board[kingPos] != 'q' or board[kingPos] != 'r':
+            elif (board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'r') and board[kingPos[0], kingPos[1]] != '':
                 return inCheck
             else:
                 inCheck = True
                 break
     return inCheck
 
+def isKingInCheckOnDiagonal(board, kingPos, colour): 
+    inCheck = False
+    kingPos = list(kingPos)
+    if colour == 'w':
+        #check below to the left
+        while kingPos[0] - 1 > 0 and kingPos[1] - 1 > 0:
+            kingPos[0] -= 1
+            kingPos[1] -= 1
+            if board[kingPos[0], kingPos[1]] == '': 
+                continue
+            elif (board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'b' or board[kingPos[0], kingPos[1]] != 'p') and board[kingPos[0], kingPos[1]] != '':
+                return inCheck
+            else:
+                inCheck = True
+                break
+        #check below to the right
+        while kingPos[0] - 1 > 0 and kingPos[1] + 1 <= 8:
+            kingPos[0] -= 1
+            kingPos[1] += 1
+            if board[kingPos[0], kingPos[1]] == '': 
+                continue
+            elif (board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'b' or board[kingPos[0], kingPos[1]] != 'p') and board[kingPos[0], kingPos[1]] != '':
+                return inCheck
+            else:
+                inCheck = True
+                break
+        #check above to the right
+        while kingPos[0] + 1 <= 8 and kingPos[1] + 1 <= 8:
+            kingPos[0] += 1
+            kingPos[1] += 1
+            if board[kingPos[0], kingPos[1]] == '': 
+                continue
+            elif board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'b' or board[kingPos[0], kingPos[1]] != 'p' or board[kingPos[0], kingPos[1]] != '':
+                return inCheck
+            else:
+                inCheck = True
+                break
+        #check above to the left
+        while kingPos[0] + 1 <= 8 and kingPos[1] - 1 <= 8:
+            kingPos[0] += 1
+            kingPos[1] += 1
+            if board[kingPos[0], kingPos[1]] == '': 
+                continue
+            elif board[kingPos[0], kingPos[1]] != 'q' or board[kingPos[0], kingPos[1]] != 'b' or board[kingPos[0], kingPos[1]] != 'p'or board[kingPos[0], kingPos[1]] != '': #FIX because pawns only have one range also fix pawns only attacking in one direction!
+                return inCheck
+            else:
+                inCheck = True
+                break
+
+    return inCheck
+
+# def isKingInCheckFromKnight(board, WKing, colour): 
+
+def printBoard(board):
+    for i in range(1, 9):
+        for j in range(1, 9):
+            if not board[(i, j)] == '': 
+                print("| " + board[(i, j)], end=" |")
+            else:
+                print("|  ", end=" |")
+        print()
+
 pieces = generateRandomPieceSet()
 board = generateboard()
-print(placepieces(board, pieces))
+placepieces(board, pieces)
+
+printBoard(board)
+# print(placepieces(board, pieces))
+print(isWhiteKingInCheck(board))
+
